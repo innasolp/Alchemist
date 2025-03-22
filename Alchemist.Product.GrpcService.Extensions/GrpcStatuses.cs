b41221a70b224cea4d28371c3a4c823bca8879ae
innasolp <innasolp@gmail.com>
2025-03-22 10:54:02 +0300

    ALCHEMIST-044 add badrequest statuses to grpcservice methods

---
+++ Alchemist.Product.GrpcService.Extensions/GrpcStatuses.cs
@@ -0,0 +1,42 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Google.Rpc;
+using Grpc.Core;
+
+namespace Alchemist.Product.GrpcService.Extensions;
+
+public static class GrpcStatuses
+{
+    public static Grpc.Core.Status GetBadRequestRpcStatus(string paramName)
+    {
+        BadRequest badRequest = new();
+        badRequest.FieldViolations.Add(new BadRequest.Types.FieldViolation { Field = paramName, Description = "Value is null or empty" });
+
+        return new Grpc.Core.Status(StatusCode.InvalidArgument, Any.Pack(badRequest).Value.ToBase64());   
+    }
+
+    public static RpcException GetBadRequestRpcException(string paramName)
+    {
+        var status = new Google.Rpc.Status() { Code = (int)StatusCode.InvalidArgument };
+        status.Details.Add(Any.Pack(new BadRequest
+        {
+            FieldViolations =
+                        {
+                            new BadRequest.Types.FieldViolation { Field = paramName, Description = "Value is null or empty" }
+                        }
+        }));
+        return status.ToRpcException();
+    }
+
+    public static RpcException GetBadRequestRpcException(string paramName, string message)
+    {
+        var status = new Google.Rpc.Status() { Code = (int)StatusCode.InvalidArgument };
+        status.Details.Add(Any.Pack(new BadRequest
+        {
+            FieldViolations =
+                        {
+                            new BadRequest.Types.FieldViolation { Field = paramName, Description = message }
+                        }
+        }));
+        return status.ToRpcException();
+    }
+}
