6fb000cd8e785c0e162e83697bc31df084c14915
innasolp <innasolp@gmail.com>
2025-03-22 10:55:46 +0300

    ALCHEMIST-044 add base fixtures for integration testing

---
+++ Alchemist.Test.Server.Fixtures/StartupTestFixture.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace Alchemist.Test.Server.Fixtures;
+
+//https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/grpc/test-services/sample/Tests/Server/IntegrationTests/Helpers/GrpcTestFixture.cs
+public delegate void LogMessage(LogLevel logLevel, string categoryName, EventId eventId, string message, Exception? exception);
+
+public class StartupTestFixture<TStartup> : IClassFixture<TStartup>, IDisposable where TStartup : class
+{
+    private TestServer? _server;
+    private IHost? _host;
+    private HttpMessageHandler? _handler;
+    private Action<IWebHostBuilder>? _configureWebHost;
+
+    public event LogMessage? LoggedMessage;
+
+    public StartupTestFixture()
+    {
+        LoggerFactory = new LoggerFactory();
+        LoggerFactory.AddProvider(new ForwardingLoggerProvider((logLevel, category, eventId, message, exception) =>
+        {
+            LoggedMessage?.Invoke(logLevel, category, eventId, message, exception);
+        }));
+    }
+
+    public void ConfigureWebHost(Action<IWebHostBuilder> configure)
+    {
+        _configureWebHost = configure;
+    }
+
+    private void EnsureServer()
+    {
+        if (_host == null)
+        {
+            var builder = new HostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<ILoggerFactory>(LoggerFactory);
+                })
+                .ConfigureWebHostDefaults(webHost =>
+                {
+                    webHost
+                        .UseTestServer()
+                        .UseStartup<TStartup>();
+
+                    _configureWebHost?.Invoke(webHost);
+                });
+            _host = builder.Start();
+            _server = _host.GetTestServer();
+            _handler = _server.CreateHandler();
+        }
+    }
+
+    public LoggerFactory LoggerFactory { get; }
+
+    public HttpMessageHandler Handler
+    {
+        get
+        {
+            EnsureServer();
+            return _handler!;
+        }
+    }
+
+    public void Dispose()
+    {
+        _handler?.Dispose();
+        _host?.Dispose();
+        _server?.Dispose();
+    }
+
+    public IDisposable GetTestContext(ITestOutputHelper outputHelper)
+    {
+        return new TestContext<TStartup>(this, outputHelper);
+    }
+}
---
+++ Alchemist.Test.Server.Fixtures/WebAppFactoryTestFixture.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Alchemist.Test.Server.Fixtures;
+
+public class WebAppFactoryTestFixture<TWebAppFactory, TEntryPoint> : IClassFixture<TWebAppFactory>
+    where TEntryPoint : Program
+    where TWebAppFactory : WebApplicationFactory<TEntryPoint>
+{
+}
