c023d6ee604016009cb5f69baeecf82f5198982a
innasolp <innasolp@users.noreply.github.com>
2025-07-12 08:56:28 +0300

    ALCHEMIST-086 partial view for modal dialog

---
+++ Alchemist.Product.Import.WebApp/Models/ShowModalModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Alchemist.Product.Import.WebApp.Models;
+
+public class ShowModalModel(string modalDivClass, string modalBodyDivClass)
+{
+    public string ModalDivClass { get; } = modalDivClass;
+    public string ModalBodyDivClass { get; } = modalBodyDivClass;
+}
