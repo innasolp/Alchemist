ca3e63ef4d8edf0e9dd94096eeed2698c555565e
innasolp <innasolp@gmail.com>
2025-09-20 13:51:06 +0300

    ALCHEMIST-122 shop model and shop webapp added

---
+++ Alchemist.Product.Import.ShopFacade/Infrastructure/ShopModelExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Alchemist.Product.Interfaces;
+
+namespace Alchemist.Product.Import.Model.Shop.Infrastructure;
+
+public static class ShopModelExtensions
+{
+    public static void Update(this IShopModel target, IShopModel shop)
+    {
+        target.Name = shop.Name;
+        target.Url = shop.Url;
+        target.Caption = shop.Caption;
+        target.IsDeprecated = shop.IsDeprecated;
+    }
+
+    public static void SetFrom(this IShopModel target, IShop shop)
+    {
+        target.Name = shop.Name;
+        target.Url = shop.Url;
+        target.Caption = shop.Caption;
+    }
+}
---
+++ Alchemist.Product.Import.ShopWebApp/Controllers/ShopApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alchemist.Product.ShopWebApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ShopApiController : Controller
+{
+}
