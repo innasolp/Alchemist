88e422db1c20cd80c28c0b1f22173f82d6ba0911
innasolp <innasolp@gmail.com>
2026-04-10 13:06:05 +0300

    #366 obsolete webapp factories removed

--- Shop.API.Test/Infrastructure/ShopAPIContextTestFixture.cs
+++ Shop.API.Test/Infrastructure/ShopAPIContextTestFixture.cs
@@ -1,16 +1,8 @@
-﻿using Alchemist.Product.Data;
-using Alchemist.Test.DBApiWebAppFactory.Configuration;
-using Alchemist.Test.DBApiWebAppFactory.Context;
-using Alchemist.Test.Server.Fixtures;
+﻿using Alchemist.Test.Server.Fixtures;
 using Xunit.Abstractions;
 
 namespace Shop.API.Test.Infrastructure;
 
-public class ShopAPIContextTestFixture<TWebAppFactory>(TWebAppFactory webAppFactory, ITestOutputHelper outputHelper) 
-    : TestFixture<TWebAppFactory, ShopAPIProgram>(webAppFactory, outputHelper)
-    where TWebAppFactory: DbContextWebAppFactory<ShopAPIProgram, AlchemyContext>
-{
-}
 public class ShopAPIConfigurationTestFixture<TWebAppFactory>(TWebAppFactory webAppFactory, ITestOutputHelper outputHelper) 
     : TestFixture<TWebAppFactory, ShopAPIProgram>(webAppFactory, outputHelper)
     where TWebAppFactory: TestHostServerWebAppFactory<ShopAPIProgram>
